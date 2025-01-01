32441017a4619bbb51c209da4ae9f66f1fe281f4
sumit-kevlani-0830 <sumitkevlani0830@gmail.com>
2025-01-01 21:54:45 +0530

    Time: 0 ms (100%), Space: 9.4 MB (15.07%) - LeetHub

--- 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.cpp
+++ 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.cpp
@@ -2,20 +2,14 @@ class Solution {
 public:
     int maxChunksToSorted(vector<int>& arr) {
         int n = arr.size();
-        vector<int>preMax(n,0);
-        vector<int>sufMin(n,0);
+        int maxi = 0;
+        int chunks = 0;
         for(int i = 0;i < n;i++){
-            preMax[i] = ((i==0)?arr[i]:max(preMax[i-1],arr[i]));
-        }
-        for(int i = n-1;i >= 0;i--){
-            sufMin[i] = ((i==n-1)?arr[i]:min(sufMin[i+1],arr[i]));
-        }
-        int ans = 1;
-        for(int i = 0;i < n-1;i++){
-            if(preMax[i]<=sufMin[i+1]){
-                ans++;
+            maxi = max(maxi,arr[i]);
+            if(maxi<=i){
+                chunks++;
             }
         }
-        return ans;
+        return chunks;
     }
 };
